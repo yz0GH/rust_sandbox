c2091fc50e6a998edafda422bbac503a250aa4e0
yz0 <yz0@tutamail.com>
2024-06-05 15:51:55 +0100

    circle in bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bevy::prelude::*;
+use bevy::render::mesh::shape::Circle;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::DefaultPlugins;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, init)
+        .run()
+}
+
+fn init(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: bevy::sprite::Mesh2dHandle(meshes.add(Circle {
+            radius: 20.0,
+            vertices: 50,
+        })),
+        material: materials.add(Color::rgb(255.0, 255.0, 255.0)),
+        transform: Transform::from_xyz(30.0, 30.0, 0.0),
+        ..default()
+    });
 }
